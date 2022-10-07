1374e83737490917c6f750fa7ae0fff5ecbaa73a
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-10-07 18:54:36 -0300

    skeleton done

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+accept_all = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=accept_all,
+    allow_credentials=True,
+    allow_methods=accept_all,
+    allow_headers=accept_all,
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Welcome to": "Backoffice API Gateway"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
